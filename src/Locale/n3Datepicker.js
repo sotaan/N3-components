206d8aea70a33dc78a38766e9567243baf5ed020
David Mamane <sotaan@users.noreply.github.com>
2017-05-29 14:24:26 +0200

    Added french locales

--- src/Locale/n3Datepicker.js
+++ src/Locale/n3Datepicker.js
@@ -1,78 +1,97 @@
 module.exports = {
-   'sun': {
-        'zh':'日',
-        'en':'Su'
-   },
-   'mon': {
-        'zh':'一',
-        'en':'Mo'
-   },
-   'tues': {
-        'zh':'二',
-        'en':'Tu'
-   },
-   'wednes': {
-        'zh':'三',
-        'en':'We'
-   },
-   'thurs': {
-        'zh':'四',
-        'en':'Th',
-   },
-   'fri': {
-        'zh':'五',
-        'en':'Fr'
-   },
-   'satur': {
-        'zh':'六',
-        'en':'Sa'
-   },
-   'jan': {
-        'zh':'一月',
-        'en':'Jan'
-   },
-   'feb': {
-        'zh':'二月',
-        'en':'Feb'
-   },
-   'mar': {
-        'zh':'三月',
-        'en':'Mar'
-   },
-   'apr': {
-        'zh':'四月',
-        'en':'Apr'
-   },
-   'may': {
-        'zh':'五月',
-        'en':'May'
-   },
-   'jun': {
-        'zh':'六月',
-        'en':'Jun'
-   },
-   'jul': {
-        'zh':'七月',
-        'en':'Jul'
-   },
-   'aug': {
-        'zh':'八月',
-        'en':'Aug'
-   },
-   'sep': {
-        'zh':'九月',
-        'en':'Sep'
-   },
-   'oct': {
-        'zh':'十月',
-        'en':'Oct'
-   },
-   'nov': {
-        'zh':'十一月',
-        'en':'Nov'
-   },
-   'dec': {
-        'zh':'十二月',
-        'en':'Dec'
-   },
+  'sun': {
+       'zh':'日',
+       'en':'Su',
+       'fr':'Di'
+  },
+  'mon': {
+       'zh':'一',
+       'en':'Mo',
+       'fr':'Lu'
+  },
+  'tues': {
+       'zh':'二',
+       'en':'Tu',
+       'fr':'Ma'
+  },
+  'wednes': {
+       'zh':'三',
+       'en':'We',
+       'fr':'Me'
+  },
+  'thurs': {
+       'zh':'四',
+       'en':'Th',
+       'fr':'Je'
+  },
+  'fri': {
+       'zh':'五',
+       'en':'Fr',
+       'fr':'Ve'
+  },
+  'satur': {
+       'zh':'六',
+       'en':'Sa',
+       'fr':'Sa'
+  },
+  'jan': {
+       'zh':'一月',
+       'en':'Jan',
+       'fr':'Janvier'
+  },
+  'feb': {
+       'zh':'二月',
+       'en':'Feb',
+       'fr':'Février'
+  },
+  'mar': {
+       'zh':'三月',
+       'en':'Mar',
+       'fr':'Mars'
+  },
+  'apr': {
+       'zh':'四月',
+       'en':'Apr',
+       'fr':'Avril'
+  },
+  'may': {
+       'zh':'五月',
+       'en':'May',
+       'fr':'Mai'
+  },
+  'jun': {
+       'zh':'六月',
+       'en':'Jun',
+       'fr':'Juin'
+  },
+  'jul': {
+       'zh':'七月',
+       'en':'Jul',
+       'fr':'Juillet'
+  },
+  'aug': {
+       'zh':'八月',
+       'en':'Aug',
+       'fr':'Aout'
+  },
+  'sep': {
+       'zh':'九月',
+       'en':'Sep',
+       'fr':'Septembre'
+  },
+  'oct': {
+       'zh':'十月',
+       'en':'Oct',
+       'fr':'Octobre'
+  },
+  'nov': {
+       'zh':'十一月',
+       'en':'Nov',
+       'fr':'Novembre'
+  },
+  'dec': {
+       'zh':'十二月',
+       'en':'Dec',
+       'fr':'Décembre'
+  },
 }
